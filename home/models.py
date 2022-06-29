841d385c37267bda2db8c977789379abdb8f8b26
Crowdbotics <team@crowdbotics.com>
2022-06-29 12:31:10 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,1 +1,16 @@
+from django.conf import settings
 from django.db import models
+
+
+class Persons(models.Model):
+    "Generated Model"
+    firstName = models.TextField()
+    lastName = models.CharField(
+        max_length=256,
+    )
+    dateOfBirth = models.DateField()
+    user = models.OneToOneField(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="persons_user",
+    )
--- myplatform_35968/wsgi.py
+++ myplatform_35968/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myplatform_35968.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myplatform_35968.settings")
 
 application = get_wsgi_application()
